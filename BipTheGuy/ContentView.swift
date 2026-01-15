3d0da496ee2d6247e615a232f082490284dadbf2
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-15 09:02:39 -0800

    Ch 3.1 Added launch scrren, app icon, simple image with sound play, button for future photo picker usage.

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,47 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer : AVAudioPlayer!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound("punchSound")
+                }
+            
+            Spacer()
+            
+            Button {
+                //TODO: Button action here
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+
         }
         .padding()
+    }
+    
+    func playSound(_ name: String) {
+        if audioPlayer != nil, audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: name) else { return }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        }
+        catch {
+            print("Issue occured during sound play: \n\(error)")
+        }
     }
 }
 
